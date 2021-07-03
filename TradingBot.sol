ddc01d1f563339cf45118092816179814cfdb69a
xTeKc <bcd3v@pm.me>
2021-07-03 11:51:01 -0400

    add action type

--- TradingBot.sol
+++ TradingBot.sol
@@ -5,3 +5,17 @@ interface Structs {
         uint256 value;
     }
 }
+
+    enum ActionType {
+      Deposit,   // supply tokens
+      Withdraw,  // borrow tokens
+      Transfer,  // transfer balance between accounts
+      Buy,       // buy an amount of some token (externally)
+      Sell,      // sell an amount of some token (externally)
+      Trade,     // trade tokens against another account
+      Liquidate, // liquidate an undercollateralized or expiring account
+      Vaporize,  // use excess tokens to zero-out a completely negative account
+      Call       // send arbitrary data to an address
+    }
+
+    
