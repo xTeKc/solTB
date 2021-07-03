f0e9126927f81a8b74967810ce587bfca62fe9d9
xTeKc <bcd3v@pm.me>
2021-07-03 12:20:28 -0400

    server config

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ const axios = require('axios')
 const moment = require('moment-timezone')
 const numeral = require('numeral')
 const _ = require('lodash')
+
+// SERVER CONFIG
+const PORT = process.env.PORT || 5000
+const app = express();
+const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors({credentials: true, origin: '*'}))
+
